2e77b65b2065c3ca29f914d66e5f85e128884941
devanshmesson <devansh.messon@kongsbergdigital.com>
2023-10-07 05:37:40 +0530

    Added CRUD operations for Product class

--- Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -8,7 +8,9 @@ namespace Bulky.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork
     {
-        public ICategoryRepository Category { get; }  
+        public ICategoryRepository Category { get; }
+        public IProductRepository Product { get;}
+
 
         void Save();
     }
---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var product = _unitOfWork.Product.GetAll().ToList();
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (int.TryParse(obj.Title, out _))
+            {
+                ModelState.AddModelError("Title", "Title cannot be a number");
+            }
+            if (ModelState.IsValid) 
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id != null || id > 0)
+            {
+                var obj = _unitOfWork.Product.Get(x => x.Id == id);
+                return View(obj);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = _unitOfWork.Product.Get(x=>x.Id == id);
+            if(product != null)
+            {
+                _unitOfWork.Product.Remove(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product deleted successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id != null || id > 0)
+            {
+                var obj = _unitOfWork.Product.Get(x=>x.Id == id);
+                return View(obj);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if(int.TryParse(obj.Title,out _))
+            {
+                ModelState.AddModelError("Title", "Title cannot be null");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product details updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
