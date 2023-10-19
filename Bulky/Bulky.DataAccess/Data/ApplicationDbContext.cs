a95f06b279df5c7dbab53ebc1eadd403c35796ed
devanshmesson <devansh.messon28@outlook.com>
2023-10-20 01:15:39 +0530

    added identity changes again

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Bulky.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.DataAccess.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
@@ -18,7 +19,7 @@ namespace Bulky.DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //configuration for IdentityDbContext
-            //base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
