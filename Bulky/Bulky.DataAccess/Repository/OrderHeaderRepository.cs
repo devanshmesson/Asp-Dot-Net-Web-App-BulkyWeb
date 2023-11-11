fc8f44e41adad76bdc52eacb99cefcbfdebbb2c7
devanshmesson <devansh.messon28@outlook.com>
2023-11-12 00:24:48 +0530

    Writing logic for updating order status & Update Stripe PaymentID

--- Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -21,5 +21,35 @@ namespace Bulky.DataAccess.Repository
         {
             _db.OrderHeaders.Update(orderHeader);
         }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderHeader = _db.OrderHeaders.FirstOrDefault(orderHeader => orderHeader.Id == id);
+            if (orderHeader != null)
+            {
+                orderHeader.OrderStatus = orderStatus;
+                if(!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderHeader.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var orderHeader = _db.OrderHeaders.FirstOrDefault(orderHeader => orderHeader.Id == id);
+            if (orderHeader != null)
+            {
+                if (!string.IsNullOrEmpty(sessionId))
+                {
+                    orderHeader.SessionId = sessionId;
+                }
+                if (!string.IsNullOrEmpty(paymentIntentId))
+                {
+                    orderHeader.PaymentIntentId = paymentIntentId;
+                    orderHeader.PaymentDate = DateTime.Now;
+                }
+            }
+        }
     }
 }
