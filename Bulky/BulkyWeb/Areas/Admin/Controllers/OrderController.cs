feddb791cb78274afd7f444abc3d444c56d1adc5
devanshmesson <devansh.messon28@outlook.com>
2023-11-15 01:23:27 +0530

    Created Order Controller for displaying all orders

---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var product = _unitOfWork.OrderHeader.GetAll(IncludeProperties: "ApplicationUser").ToList();
+            return Json(new { data = product });
+
+        }
+        #endregion
+    }
+}
