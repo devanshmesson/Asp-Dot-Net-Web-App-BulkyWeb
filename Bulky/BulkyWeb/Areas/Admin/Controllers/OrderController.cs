82366d25b9f274e83fb690374b775dd8d7ed8ac1
devanshmesson <devansh.messon28@outlook.com>
2023-11-16 01:59:19 +0530

    Display Order List

--- Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         #region API Calls
