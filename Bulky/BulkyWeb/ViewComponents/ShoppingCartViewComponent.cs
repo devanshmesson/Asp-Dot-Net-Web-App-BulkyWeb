e276b8b88caec84a998e56935ba9d1ca37e4e0d3
devanshmesson <devansh.messon28@outlook.com>
2023-11-25 22:42:42 +0530

    Implemented View Component for shopping cartt

---
+++ Bulky/BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        public IUnitOfWork _unitOfWork { get; set; }
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var user = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (user != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == user).Count());
+
+                }
+
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+
+
+
+        }
+    }
+}
