b13d1c16f29c9b77faf6b2d68fb4a66451cb59bb
devanshmesson <devansh.messon28@outlook.com>
2023-12-11 17:14:01 -0500

    Implemented Update functionality in Manage user option.

---
+++ Bulky.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bulky.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Areas.Admin.Controllers
@@ -13,14 +16,72 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         public ApplicationDbContext _db;
-        public UserController(ApplicationDbContext db)
+        public UserManager<IdentityUser> _userManager;
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult RoleManagement(string? userId)
+        {
+            string roleId = _db.UserRoles.FirstOrDefault(x => x.UserId == userId).RoleId;
+            var user = _db.ApplicationUser.Include(x=> x.Company).FirstOrDefault(x => x.Id == userId);
+            var roleList = _db.Roles.ToList();
+            var companyList = _db.Companies.ToList();
+
+            RoleManagementVM roleManagementVM = new()
+            {
+                ApplicationUser = user,
+                RoleList = roleList.Select(role => new SelectListItem
+                {
+                    Text = role.Name,
+                    Value = role.Name
+                }),
+                CompanyList = companyList.Select(company => new SelectListItem
+                {
+                    Text = company.Name,
+                    Value = company.Id.ToString()
+                })
+
+            };
+            roleManagementVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(x => x.Id == roleId).Name;
+            return View(roleManagementVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            var oldRoleId = _db.UserRoles.FirstOrDefault(x => x.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            var oldRoleName = _db.Roles.FirstOrDefault(x => x.Id == oldRoleId).Name;
+            var newRoleName = roleManagementVM.ApplicationUser.Role;
+            ApplicationUser user = _db.ApplicationUser.FirstOrDefault(x => x.Id == roleManagementVM.ApplicationUser.Id);
+
+            if(oldRoleName != newRoleName)
+            {
+                if(newRoleName == SD.Role_Company)
+                {
+                    user.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if(oldRoleName == SD.Role_Company)
+                {
+                    user.CompanyId = null;
+                }
+                _db.SaveChanges();
+                _userManager.RemoveFromRoleAsync(user, oldRoleName).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(user, newRoleName).GetAwaiter().GetResult();
+
+            }
+                return RedirectToAction("Index");
+
+
+        }
+
+
         #region APICalls
         [HttpGet]
         public IActionResult GetAll()
@@ -44,6 +105,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
             return Json(new { data = users });
         }
+
+      
 
         [HttpPost]
         public IActionResult LockUnlock([FromBody] string userId)
